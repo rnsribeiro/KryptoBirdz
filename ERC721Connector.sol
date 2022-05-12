5983f85d4d048918361cb3025d2ed6ece0d9d3f2
Rodrigo Nunes <rnsribeiro@gmail.com>
2022-05-12 09:58:26 -0300

    Add files via upload

--- ERC721Connector.sol
+++ ERC721Connector.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "https://github.com/rnsribeiro/KryptoBirdz/blob/main/ERC721Metadata.sol";
+import "https://github.com/rnsribeiro/KryptoBirdz/blob/main/ERC721.sol";
 
 contract ERC721Connector is ERC721Metadata {
     constructor(string memory name, string memory symbol) ERC721Metadata(name, symbol){
